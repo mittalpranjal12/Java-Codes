2b6f5b43fa5a8fdeb41dc3236976cd6a42037ffd
pranjal mittal <pranjalmittal61117@gmail.com>
2020-05-21 21:16:57 +0530

    Update if_else.java

--- if_else.java
+++ if_else.java
@@ -1,3 +1,8 @@
+// Program that takes a character as input and prints either 1, 0 or -1 according to the following rules.
+//1, if the character is an uppercase alphabet (A - Z)
+//0, if the character is a lowercase alphabet (a - z)
+//-1, if the character is not an alphabet
+
 import java.util.Scanner;
 public class if_else {
 

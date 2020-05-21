f83826d1f31072af8660e789321720671278a218
pranjal mittal <pranjalmittal61117@gmail.com>
2020-05-21 21:10:56 +0530

    Add files via upload

---
+++ Conversion.java
@@ -0,0 +1,15 @@
+import java.util.Scanner;
+public class Conversion {
+
+	public static void main(String[] args) {
+		Scanner s = new Scanner(System.in);
+		
+		int fah = s.nextInt();
+		
+		int cel = (5*(fah - 32))/9;
+		
+		System.out.println(cel);
+		s.close();
+	}
+
+}

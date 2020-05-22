c60a8531184bb65528c2a89c398130d520fa536d
pranjal mittal <pranjalmittal61117@gmail.com>
2020-05-22 22:50:02 +0530

    Update Average_Marks.java

--- Average_Marks.java
+++ Average_Marks.java
@@ -1,3 +1,7 @@
+// marks of three tests as m1, m2, and m3 of a student considering all the three marks have been given in integer format.
+// We need to calculate the average of the given marks and print it.
+//All the test marks are in integers and hence calculate the average in integer as well.
+
 import java.util.Scanner;
 public class Average_Marks {
 

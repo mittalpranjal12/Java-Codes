f90001e0b1b9a4d30675d8025d42d6af1b767258
pranjal mittal <pranjalmittal61117@gmail.com>
2020-05-22 22:47:43 +0530

    Update Fah_to_Cel_table.java

--- Fah_to_Cel_table.java
+++ Fah_to_Cel_table.java
@@ -1,5 +1,5 @@
 //Given three values - Start Fahrenheit Value (S), End Fahrenheit value (E) and Step Size (W), 
-//you need to convert all Fahrenheit values from Start to End at the gap of W, into their corresponding Celsius values and print the table.
+//Convert all Fahrenheit values from Start to End at the gap of W, into their corresponding Celsius values and print the table.
 import java.util.Scanner;
 public class Fah_to_Cel_table {
 

0a8eb54205c8323d84b1cb41fad5c0305867b879
pranjal mittal <pranjalmittal61117@gmail.com>
2020-05-23 19:42:14 +0530

    Update total_salary.java

--- total_salary.java
+++ total_salary.java
@@ -1,3 +1,15 @@
+//WAP to calculate the total salary of a person. The user has to enter the basic salary (an integer) and 
+//the grade (an uppercase character), and depending upon which the total salary is calculated as -
+   // totalSalary = basic + hra + da + allow – pf
+// where :
+// hra   = 20% of basic
+// da    = 50% of basic
+// allow = 1700 if grade = ‘A’
+// allow = 1500 if grade = ‘B’
+// allow = 1300 if grade = ‘C' or any other character
+// pf    = 11% of basic.
+//Round off the total salary and then print the integral part only.
+
 package Introduction;
 
 import java.util.Scanner;
@@ -7,6 +19,7 @@ public class total_salary {
 	public static void main(String[] args) {
 		Scanner s = new Scanner(System.in);
         
+		System.out.print("Enter the basic salary and a character: ");
         double basic = s.nextInt();
         String str = s.next();
         char ch = str.charAt(0);
@@ -30,7 +43,7 @@ public class total_salary {
             
         }
          double totalSalary = basic + hra + da + allow - pf;
-            System.out.println(Math.round(totalSalary));
+            System.out.println("Total Salary: " + Math.round(totalSalary));
             
             s.close();
 	}

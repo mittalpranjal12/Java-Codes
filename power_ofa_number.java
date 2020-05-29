4deb33937a3f86c7ed70f5f13207446ac66825fb
pranjal mittal <pranjalmittal61117@gmail.com>
2020-05-29 16:04:33 +0530

    Add files via upload

---
+++ power_ofa_number.java
@@ -0,0 +1,21 @@
+package For_loop;
+
+import java.util.Scanner;
+
+public class power_ofa_number {
+
+	public static void main(String[] args) {
+		Scanner s = new Scanner(System.in);
+        int ans = 1;
+        int x = s.nextInt();
+        int n = s.nextInt();
+        
+        for(int i = 1; i<=n; i++){
+            ans = ans*x;
+        }
+        System.out.print(ans);
+        
+        s.close();
+	}
+	}
+

d00dc57bacf4b7f0dfde55c263fcb10753a4dfbb
pranjal mittal <pranjalmittal61117@gmail.com>
2020-06-09 22:26:37 +0530

    Update mirror_num_pattern.java

--- mirror_num_pattern.java
+++ mirror_num_pattern.java
@@ -1,3 +1,11 @@
+// Print the following pattern for the given N number of rows.
+// Pattern for N = 4
+// ...1
+// ..12
+// .123
+// 1234
+// The dots represent spaces.
+
 package Patterns;
 
 import java.util.Scanner;

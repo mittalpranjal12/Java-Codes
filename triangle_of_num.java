6387bef9d2b5c6baf87bcaabaed7e3d4a5e1c88c
pranjal mittal <pranjalmittal61117@gmail.com>
2020-06-11 23:54:38 +0530

    Add files via upload

---
+++ triangle_of_num.java
@@ -0,0 +1,42 @@
+package Patterns;
+
+import java.util.Scanner;
+
+public class triangle_of_num {
+
+	public static void main(String[] args) {
+		Scanner s = new Scanner(System.in);
+        int n = s.nextInt();
+        int i = 1;
+        
+        while( i <= n) {
+        	 int spaces = 1;
+        	 int p = i;
+	            
+         	while( spaces <= n - i ){
+                 System.out.print(" ");
+                 spaces++;
+             }
+         	int j = 1;
+         	
+         		while( j <= i) {
+         			System.out.print(p);
+         			p++;
+         			j++;
+         		}
+         		p = 2*i - 2;
+         	j = 1;
+         		while( j <= i - 1 ) {
+         			
+         			System.out.print(p);
+         			p--;
+         			j++;
+
+        		}
+         		System.out.println();
+	            i++;
+        }
+        s.close();
+	}
+
+}

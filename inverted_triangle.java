4cc403dbbf21fe3b45bc5ee56015605d31c4680b
pranjal mittal <pranjalmittal61117@gmail.com>
2020-06-18 14:25:14 +0530

    Add files via upload

---
+++ inverted_triangle.java
@@ -0,0 +1,26 @@
+package Patterns;
+
+import java.util.Scanner;
+
+public class inverted_triangle {
+
+	public static void main(String[] args) {
+		Scanner s = new Scanner(System.in);
+		
+		int n = s.nextInt();
+		int i = 1;
+        
+        while( i <= n){
+            int j = 1;
+            
+            while( j <= n - i + 1) {
+            	System.out.print("*");
+            	j++;
+            }
+            System.out.println();
+            i++;
+        }
+        	s.close();
+	}
+
+}

28f0f4439424fbdef4d93466f9569e88aacd280c
pranjal mittal <pranjalmittal61117@gmail.com>
2020-06-19 11:40:04 +0530

    Update sum_or_product_choice.java

--- sum_or_product_choice.java
+++ sum_or_product_choice.java
@@ -1,3 +1,13 @@
+// Write a program that asks the user for a number N and a choice C. 
+// And then give them the possibility to choose between computing the sum and computing the product of all integers in the range 1 to N (both inclusive).
+// If C is equal to -
+//  1, then print the sum
+//  2, then print the product
+//  Any other number, then print '-1' (without the quotes)
+//   10
+//   1
+// output: 55
+
 package For_loop;
 
 import java.util.Scanner;

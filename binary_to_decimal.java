1d88737f73078279c641232cfcb15084b5b900a9
pranjal mittal <pranjalmittal61117@gmail.com>
2020-06-21 22:30:46 +0530

    Add files via upload

---
+++ binary_to_decimal.java
@@ -0,0 +1,23 @@
+package For_loop;
+
+import java.util.Scanner;
+
+public class binary_to_decimal {
+
+	public static void main(String[] args) {
+		Scanner s = new Scanner(System.in);
+		int n = s.nextInt();
+		int pv = 1;
+		int ans = 0;
+		
+		while(n!= 0) {
+			int digit = n%10;
+			ans = ans + digit*pv;
+			pv = pv*2;
+			n = n/10;
+		}
+		System.out.println(ans);
+		s.close();
+	}
+
+}

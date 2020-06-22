5b46818d4b85e536c165f1e830afa1e613c590de
pranjal mittal <pranjalmittal61117@gmail.com>
2020-06-22 21:47:02 +0530

    Update square_root_integral.java

--- square_root_integral.java
+++ square_root_integral.java
@@ -1,3 +1,8 @@
+// Given a number N, find its square root. You need to find and print only the integral part of square root of N.
+// For eg. if number given is 18, answer is 4.
+// N = 4 ans:2
+// N = 10 ans: 3
+
 package For_loop;
 
 import java.util.Scanner;

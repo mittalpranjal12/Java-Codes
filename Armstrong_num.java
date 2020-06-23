0d1e8b6ee21c0b9acca07cd2501422d1fd486898
pranjal mittal <pranjalmittal61117@gmail.com>
2020-06-23 23:39:26 +0530

    Update Armstrong_num.java

--- Armstrong_num.java
+++ Armstrong_num.java
@@ -1,3 +1,15 @@
+// Write a Program to determine if the given number is Armstrong number or not. Print true if number is armstrong, otherwise print false.
+// An Armstrong number is a number (with digits n) such that the sum of its digits raised to nth power is equal to the number itself.
+// For example,
+// 371, as 3^3 + 7^3 + 1^3 = 371
+// 1634, as 1^4 + 6^4 + 3^4 + 4^4 = 1634
+	
+// N = 1
+// true
+	
+// N = 103
+// false
+
 package For_loop;
 
 import java.util.Scanner;

7b7bb20c07ba474128859b16580ccfef2ffa0f44
pranjal mittal <pranjalmittal61117@gmail.com>
2020-06-25 12:52:58 +0530

    Update fah_conversion.java

--- fah_conversion.java
+++ fah_conversion.java
@@ -1,3 +1,18 @@
+// Fahrenheit to Celsius Table
+// Given three values - Start Fahrenheit Value (S), End Fahrenheit value (E) and Step Size (W), you need to convert all Fahrenheit values from Start to End at the gap of W,
+// into their corresponding Celsius values and print the table.
+// Input : 
+// 0 
+// 100 
+// 20
+// Output :
+// 0   -17
+// 20  -6
+// 40  4
+// 60  15
+// 80  26
+// 100 37
+	
 package functions;
 
 import java.util.Scanner;

dcd80bc1c8410685fef6d72777c5e61e8a93b30a
pranjal mittal <pranjalmittal61117@gmail.com>
2020-06-29 17:02:13 +0530

    Update Unique_num.java

--- Unique_num.java
+++ Unique_num.java
@@ -1,3 +1,13 @@
+// You have been given an integer array/list(ARR) of size N. Where N is equal to [2M + 1].
+// Now, in the given array/list, 'M' numbers are present twice and one number is present only once.
+// You need to find and return that number which is unique in the array/list.
+// Input :
+// 1
+// 7
+// 2 3 1 6 3 6 2
+// Output:
+// 1
+
 package arrays;
 
 import java.util.Scanner;

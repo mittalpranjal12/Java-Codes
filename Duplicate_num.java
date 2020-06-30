0c365311408225ce3bd0971d3bb1cfb1191f6e0f
pranjal mittal <pranjalmittal61117@gmail.com>
2020-06-30 22:21:00 +0530

    Update Duplicate_num.java

--- Duplicate_num.java
+++ Duplicate_num.java
@@ -1,3 +1,13 @@
+// You have been given an integer array/list(ARR) of size N which contains numbers from 0 to (N - 2). Each number is present at least once. That is, if N = 5, 
+// the array/list constitutes values ranging from 0 to 3 and among these, there is a single integer value that is present twice.
+// You need to find and return that duplicate number present in the array.
+// Input:
+// 1
+// 9
+// 0 7 2 5 4 7 1 3 6
+// Output:
+// 7
+	
 package arrays;
 
 import java.util.Scanner;

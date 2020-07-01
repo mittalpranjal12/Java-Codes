75a3bd5e12f790f0e92db2e4a672a05aa9ae503b
pranjal mittal <pranjalmittal61117@gmail.com>
2020-07-01 18:32:03 +0530

    Update Array_pair_Sum.java

--- Array_pair_Sum.java
+++ Array_pair_Sum.java
@@ -1,3 +1,12 @@
+// You have been given an integer array/list(ARR) and a number X. Find and return the total number of pairs in the array/list which sum to X.
+// Input:
+// 1
+// 9
+// 1 3 6 2 5 4 3 2 4
+// 7
+// Output:
+// 7
+	
 package arrays;
 
 import java.util.Scanner;

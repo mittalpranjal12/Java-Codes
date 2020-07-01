2fbe8110d4e97605640c78c52ee617aed499e36b
pranjal mittal <pranjalmittal61117@gmail.com>
2020-07-01 18:35:29 +0530

    Update Binary_Search.java

--- Binary_Search.java
+++ Binary_Search.java
@@ -1,3 +1,17 @@
+// You have been given a sorted(in ascending order) integer array/list(ARR) of size N and an element X. Write a function to search this element in the given input array/list 
+// using 'Binary Search'. Return the index of the element in the input array/list. In case the element is not present in the array/list, then return -1.
+//  Input:
+// 7
+// 1 3 7 9 11 12 45
+// 1
+// 3
+// Output:
+// 1
+// Input Format: The first line contains an Integer 'N' which denotes the size of the array/list.
+// Second line contains 'N' single space separated integers representing the elements in the array/list.
+// Third line contains an Integer 't' which denotes the number of test cases or queries to be run. Then the test cases follow..
+// All the 't' lines henceforth, will take the value of X to be searched for in the array/list.
+
 package searching_and_sorting;
 
 import java.util.Scanner;

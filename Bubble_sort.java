6e864d46cc90d6379286732699a0e1818f3253b1
pranjal mittal <pranjalmittal61117@gmail.com>
2020-07-03 20:02:09 +0530

    Update Bubble_sort.java

--- Bubble_sort.java
+++ Bubble_sort.java
@@ -1,3 +1,14 @@
+// Provided with a random integer array/list(ARR) of size N, you have been required to sort this array using 'Bubble Sort'.
+// The first line contains an Integer 't' which denotes the number of test cases or queries to be run. Then the test cases follow.
+// First line of each test case or query contains an integer 'N' representing the size of the array/list.
+// Second line contains 'N' single space separated integers representing the elements in the array/list.
+// Input:
+// 1
+// 7
+// 2 13 4 1 3 6 28
+// Output:
+// 1 2 3 4 6 13 28
+
 package searching_and_sorting;
 
 import java.util.Scanner;

6b8dd9c86cbe05c97e7ab6c52f5b4d1600750f3e
pranjal mittal <pranjalmittal61117@gmail.com>
2020-07-06 13:51:30 +0530

    Update Check_ArrayRotation.java

--- Check_ArrayRotation.java
+++ Check_ArrayRotation.java
@@ -1,3 +1,15 @@
+// You have been given an integer array/list(ARR) of size N. It has been sorted(in increasing order) and then rotated by some number 'K' in the clockwise direction.
+// Your task is to write a function that returns the value of 'K', that means, the index from which the array/list has been rotated.
+// Input:
+// 2		//test cases
+// 5
+// 3 6 8 9 10
+// 4
+// 10 20 30 1
+// Output:
+// 0
+// 3
+
 package searching_and_sorting;
 
 import java.util.Scanner;

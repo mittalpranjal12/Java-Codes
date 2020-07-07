25dc2b5daabf3780eb116fc3e14ea0c371122c58
pranjal mittal <pranjalmittal61117@gmail.com>
2020-07-07 13:45:25 +0530

    Add files via upload

---
+++ Count_Words.java
@@ -0,0 +1,28 @@
+package strings_and_2DArrays;
+
+import java.util.Scanner;
+
+public class Count_Words {
+
+static Scanner s = new Scanner(System.in);
+	
+		public static int countWords(String str)
+		{
+			int count = 1;
+			for(int i = 0; i< str.length(); i++)
+			{
+				if(str.charAt(i) == ' ')
+				{
+					count++;
+				}
+			}
+			return count;
+		}
+	
+		
+		public static void main(String[] args) {
+		String str = s.nextLine();
+		System.out.println(Count_Words.countWords(str));
+	}
+
+}

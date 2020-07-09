d994696209daef6e155de02c74c95cdff33f1d26
pranjal mittal <pranjalmittal61117@gmail.com>
2020-07-09 22:14:03 +0530

    Update ReverseString_WordWise.java

--- ReverseString_WordWise.java
+++ ReverseString_WordWise.java
@@ -1,3 +1,10 @@
+// Reverse the given string word wise. 
+// That is, the last word in given string should come at 1st place, last second word at 2nd place and so on. Individual words should remain as it is.
+// Input :
+// Welcome to Coding Ninjas
+// Output:
+// Ninjas Coding to Welcome
+	
 package strings_and_2DArrays;
 
 import java.util.Scanner;

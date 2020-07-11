4b71e2c49f9de2a1d3367880f24fff8a6c863423
pranjal mittal <pranjalmittal61117@gmail.com>
2020-07-11 12:50:50 +0530

    Update RowWise_Sum.java

--- RowWise_Sum.java
+++ RowWise_Sum.java
@@ -1,3 +1,18 @@
+// For a given two-dimensional integer array/list of size (N x M), find and print the sum of each of the row elements in a single line, separated by a single space.
+// Input :
+// 2		//test cases
+// 2 5 
+// 4 5 3 2 6 
+// 7 5 3 8 9
+// 4 4
+// 1 2 3 4
+// 9 8 7 6
+// 3 4 5 6
+// -1 1 -10 5
+// Output :
+// 20 32 
+// 10 30 18 -5 
+
 package strings_and_2DArrays;
 
 import java.util.Scanner;

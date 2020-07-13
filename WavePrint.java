a75d95fa707eb13b9aec088b838effd4fcdaed5b
pranjal mittal <pranjalmittal61117@gmail.com>
2020-07-13 15:25:27 +0530

    Update WavePrint.java

--- WavePrint.java
+++ WavePrint.java
@@ -1,3 +1,14 @@
+// For a given two-dimensional integer array/list of size (N x M), 
+// print the array/list in a sine wave order, i.e, print the first column top to bottom, next column bottom to top and so on.
+// Input:
+// 1
+// 3 4 
+// 1  2  3  4 
+// 5  6  7  8 
+// 9 10 11 12
+// Output:
+// 1 5 9 10 6 2 3 7 11 12 8 4
+
 package strings_and_2DArrays;
 
 import java.util.Scanner;

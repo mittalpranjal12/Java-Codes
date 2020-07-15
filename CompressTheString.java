682f1e3c8e372ff02358005b4cd5c7e137f304e9
pranjal mittal <pranjalmittal61117@gmail.com>
2020-07-15 20:11:15 +0530

    Update CompressTheString.java

--- CompressTheString.java
+++ CompressTheString.java
@@ -1,3 +1,13 @@
+// Write a program to do basic string compression. For a character which is consecutively repeated more than once, 
+// replace consecutive duplicate occurrences with the count of repetitions.
+// For e.g. if a String has 'x' repeated 5 times, replace this "xxxxx" with "x5".
+// Note : Consecutive count of every character in input string is less than equal to 9.
+//  Input :
+// aaabbccdsa
+//  Output:
+// a3b2c2dsa
+
+
 package strings_and_2DArrays;
 
 import java.util.Scanner;

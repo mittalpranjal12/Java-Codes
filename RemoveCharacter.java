982ddd82eb6692d264ccf256e55bb5cf85b8aeb3
pranjal mittal <pranjalmittal61117@gmail.com>
2020-07-15 19:41:49 +0530

    Update RemoveCharacter.java

--- RemoveCharacter.java
+++ RemoveCharacter.java
@@ -1,3 +1,11 @@
+// Given a string and a character x. Write a function to remove all occurrences of x character from the given string.
+// Leave the string as it is, if the given character is not present in the string.
+// Input:
+// welcome to coding ninjas
+// o
+// Output:
+// welcme t cding ninjas
+
 package strings_and_2DArrays;
 
 import java.util.Scanner;

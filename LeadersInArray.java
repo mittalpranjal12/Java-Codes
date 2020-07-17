aaba55565aec279fcfcbee9443a5bdc7bb6a87bf
pranjal mittal <pranjalmittal61117@gmail.com>
2020-07-17 13:06:22 +0530

    Update LeadersInArray.java

--- LeadersInArray.java
+++ LeadersInArray.java
@@ -1,3 +1,17 @@
+// Given an integer array A of size n. Find and print all the leaders present in the input array. An array element A[i] is called Leader, 
+// if all the elements following it (i.e. present at its right) are less than or equal to A[i].
+// Print all the leader elements separated by space and in the same order they are present in the input array.
+// Input 1 :
+// 6
+// 3 12 34 2 0 -1
+// Output 1 :
+// 34 2 0 -1
+// Input 2 :
+// 5
+// 13 17 5 4 6
+// Output 2 :
+// 17 6
+
 package strings_and_2DArrays;
 
 import java.util.Scanner;

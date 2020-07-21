4a5382d278e3c06bec310975b6256a38e58f0462
pranjal mittal <pranjalmittal61117@gmail.com>
2020-07-21 12:55:54 +0530

    Add files via upload

---
+++ PrintNumbers.java
@@ -0,0 +1,31 @@
+package Recursion;
+
+import java.util.Scanner;
+
+public class PrintNumbers {
+	
+	public static void print(int n)
+	{
+		if(n <= 0)
+			return;
+		
+		if(n == 1)
+		{
+			System.out.print(n + " ");
+		}
+		else
+		{
+			print(n-1);
+			System.out.print(n + " ");
+		}
+	}
+
+	public static void main(String[] args) {
+		Scanner s = new Scanner(System.in);
+		
+		int n = s.nextInt();
+		print(n);
+		s.close();
+	}
+
+}

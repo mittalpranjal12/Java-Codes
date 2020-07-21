0748453d381560ba8824914b668aa094284d4916
pranjal mittal <pranjalmittal61117@gmail.com>
2020-07-21 12:41:59 +0530

    Add files via upload

---
+++ CalculatePower.java
@@ -0,0 +1,25 @@
+package Recursion;
+
+import java.util.Scanner;
+
+public class CalculatePower {
+
+	public static int power(int x, int n)
+	{
+		if(n == 0)
+			return 1;
+		
+		return x * power(x, n-1);
+	}
+	
+	static Scanner s = new Scanner(System.in);
+	
+	public static void main(String[] args) {
+
+			int x = s.nextInt();
+			int n = s.nextInt();
+			
+			System.out.println(power(x, n));
+	}
+
+}

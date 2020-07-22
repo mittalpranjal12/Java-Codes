eebbb7af4e4265a515d2b2455833c976f38a40a3
pranjal mittal <pranjalmittal61117@gmail.com>
2020-07-22 12:37:44 +0530

    Update FirstIndex_Of_Number.java

--- FirstIndex_Of_Number.java
+++ FirstIndex_Of_Number.java
@@ -1,3 +1,13 @@
+// Given an array of length N and an integer x, you need to find and return the first index of integer x present in the array. Return -1 if it is not present in the array.
+// First index means, the index of first occurrence of x in the input array.
+// Do this recursively. Indexing in the array starts from 0.
+// Input :
+// 4
+// 9 8 10 8
+// 8
+// Output :
+// 1
+
 package Recursion;
 
 import java.util.Scanner;

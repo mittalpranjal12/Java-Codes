ab6170e2fa66178cb3f8c2bdee9841624cbe41fc
pranjal mittal <pranjalmittal61117@gmail.com>
2020-07-26 14:29:38 +0530

    Update String_To_Integer.java

--- String_To_Integer.java
+++ String_To_Integer.java
@@ -1,3 +1,10 @@
+// Write a recursive function to convert a given string into the number it represents. 
+// That is input will be a numeric string that contains only numbers, you need to convert the string into corresponding integer and return the answer.
+// Input:
+// 1231
+// Output:
+// 1231
+
 package Recursion;
 
 import java.util.Scanner;

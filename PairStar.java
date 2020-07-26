0a4b7e0dca69de4359e5ad4f838774448c2d8536
pranjal mittal <pranjalmittal61117@gmail.com>
2020-07-26 14:31:25 +0530

    Update PairStar.java

--- PairStar.java
+++ PairStar.java
@@ -1,3 +1,9 @@
+// Given a string S, compute recursively a new string where identical chars that are adjacent in the original string are separated from each other by a "*".
+// Input :
+// hello
+// Output:
+// hel*lo
+
 package Recursion;
 
 import java.util.Scanner;

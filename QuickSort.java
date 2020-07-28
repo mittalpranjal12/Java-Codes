dae309c712263233c34c7c6af4e53baae2fc0192
pranjal mittal <pranjalmittal61117@gmail.com>
2020-07-28 15:16:30 +0530

    Add files via upload

---
+++ QuickSort.java
@@ -0,0 +1,68 @@
+package Recursion;
+
+import java.util.Scanner;
+
+public class QuickSort {
+
+static Scanner s = new Scanner(System.in);
+	
+	public static int[] takeInput(){
+		int size = s.nextInt();
+		int[] input = new int[size];
+		for(int i = 0; i < size; i++){
+			input[i] = s.nextInt();
+		}
+		return input;
+	}
+	
+	public static void quickSort(int[] input, int si, int ei)
+	{
+		int pivotPos = partition(input,si,ei);
+		
+		if(si < pivotPos - 1)
+			quickSort(input, si, pivotPos-1);
+		
+		if(ei > pivotPos)
+			quickSort(input, pivotPos, ei);
+	}
+	
+	public static int partition(int[] input, int si, int ei)
+	{
+		int pivot = input[si];
+		
+		while(si <= ei)
+		{
+			while(input[si] < pivot)
+			{
+					si++;
+			}
+			while(input[ei] > pivot)
+			{
+				ei--;
+			}
+			
+			if(si <= ei)
+			{
+				int temp1 = input[si];
+				input[si] = input[ei];
+				input[ei] = temp1;
+				si++;
+				ei--;
+			}
+		}
+	return si;
+}
+	
+	public static void quickSort(int[] input)
+	{
+		quickSort(input,0,input.length-1);
+	}
+	
+	public static void main(String[] args) {
+		int[] input = takeInput();
+		quickSort(input);
+		for(int i = 0; i < input.length; i++) {
+			System.out.print(input[i] + " ");
+		}
+	}
+}
---
+++ ReturnKeypad_Code.java
@@ -0,0 +1,97 @@
+package Recursion;
+
+import java.util.Scanner;
+
+public class ReturnKeypad_Code {
+
+	// T(n)=T(n/10) + k(n^2)
+		public static String[] keypad(int num) {
+
+			if(num==0) {
+				String[] ans= {""};
+				return ans;
+			}
+			String[] smallAns=keypad(num/10);
+			String[] choices= printkeypad(num%10);
+			String[] ans= new String[choices.length * smallAns.length];
+			int k=0;
+			for(int i=0;i<smallAns.length;i++) {
+				for(int j=0;j<choices.length;j++) {
+					ans[k++]= smallAns[i] + choices[j];
+				}
+			}
+			return ans;
+		}
+		
+		public static void printKeypad(int num) {
+			
+			printKeypad(num,"");
+			
+		}
+		public static void printKeypad(int num,String output) {
+			
+			if(num==0) {
+				System.out.println(output);
+				return;
+			}
+			String[] choices=keypad(num%10);
+			for(int i=0;i<choices.length;i++) {
+				printKeypad(num/10, choices[i] + output);
+			}
+
+		}
+
+
+		private static String[] printkeypad(int n)
+		{
+			if(n==0 || n==1) {
+				String[] ans= {""};
+				return ans;
+			}
+
+			else if(n==2) {
+				String[] ans= {"a","b","c"};
+				return ans;
+			}
+			else if(n==3) {
+				String[] ans= {"d","e","f"};
+				return ans;
+			}
+			else if(n==4) {
+				String[] ans= {"g","h","i"};
+				return ans;
+			}
+			else if(n==5) {
+				String[] ans= {"j","k","l"};
+				return ans;
+			}
+			else if(n==6) {
+				String[] ans= {"m","n","o"};
+				return ans;
+			}
+			else if(n==7) {
+				String[] ans= {"p","q","r","s"};
+				return ans;
+			}
+			else if(n==8) {
+				String[] ans= {"t","u","v"};
+				return ans;
+			}
+			else if(n==9) {
+				String[] ans= {"w","x","y","z"};
+				return ans;
+			}
+			return null;
+		}
+	
+	public static void main(String[] args) {
+		Scanner s = new Scanner(System.in);
+		int input = s.nextInt();
+		String output[] = keypad(input);
+		for(int i = 0; i < output.length; i++) {
+			System.out.println(output[i]);
+		}
+		s.close();
+	}
+
+}

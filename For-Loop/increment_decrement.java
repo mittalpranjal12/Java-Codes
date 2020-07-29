9259098f034baa4e9bd083a8684f207ff80ac33d
pranjal mittal <pranjalmittal61117@gmail.com>
2020-07-29 14:24:39 +0530

    Add files via upload

---
+++ For-Loop/increment_decrement.java
@@ -0,0 +1,31 @@
+package For_loop;
+
+public class increment_decrement {
+
+	public static void main(String[] args) {
+		int a = 10;
+		
+		System.out.println("a: " + a);
+		System.out.println("Post increment: " + a++);
+		System.out.println("a after post increment: " + a);
+		
+		int b = 5;
+		System.out.println("b: " + b);
+		System.out.println("Pre increment: " + (++b));
+		System.out.println("b after pre increment: " + b);
+		
+		int c= 15;
+		
+		System.out.println("c: " + c);
+		System.out.println("Post decrement: " + c--);
+		System.out.println("c after post decrement: " + c);
+		
+		int d = 20;
+		
+		System.out.println("d: " + d);
+		System.out.println("Pre decrement: " + (--d));
+		System.out.println("d after pre decrement: " + d);
+		
+	}
+
+}
---
+++ For-Loop/sum_of_n_num.java
@@ -0,0 +1,20 @@
+package For_loop;
+
+import java.util.Scanner;
+
+public class sum_of_n_num {
+
+	public static void main(String[] args) {
+		Scanner s = new Scanner(System.in);
+        int n = s.nextInt();
+        int sum = 0;
+        
+        for( int i = 1; i <=n; i++) {
+        	sum = sum + i;
+        }
+        System.out.println("Sum is: " + sum);
+        
+        s.close();
+	}
+
+}
